2d247d9fedc7fe8ad7de46b7724f511c404d091a
VladislavMazur1996 <96079740+VladislavMazur1996@users.noreply.github.com>
2023-05-21 20:10:06 +0300

    1.1.1
    1.1.1

--- pereval/pereval/settings.py
+++ pereval/pereval/settings.py
@@ -20,6 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+env = environ.Env()
+environ.Env.read_env()
+
+POSTGRES_KEY = env('POSTGRES_KEY')
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@ev^gk98_gm(gb=4)5n$%ruhggn$5b9r4tbh_)ah%8@zv9+4h4'
 
@@ -71,10 +76,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pereval.wsgi.application'
 
 
-env = environ.Env()
-environ.Env.read_env()
 
-POSTGRES_KEY = env('POSTGRES_KEY')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
